d964fe28b94208c34b8c5faff1256851c919b536
nextdesusu <nextdesusu@gmail.com>
2020-07-17 22:51:03 +0400

    init: initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainCanvasComponent } from './main-canvas/main-canvas.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainCanvasComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule
